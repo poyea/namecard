d57eb152938e884e5d4f776f1ce420f6358d5411
John Law <johnlaw.po@gmail.com>
2019-12-24 15:54:32 +0800

    Add prettier and linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: false,
+  root: true,
   env: {
-    browser: false,
-    node: false
+    browser: true,
+    node: true
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'eslint:recommended',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
   ],
   // add your custom rules here
   rules: {
+    'prettier/prettier': ['error', { 'semi': false }]
   }
 }
